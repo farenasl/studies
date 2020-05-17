87e5e45e90963dc3da231f2340e2f836cdb448ac
Fabián Arenas Loyola <farenasl@MacBook-Pro-de-Fabian.local>
2020-05-17 17:35:41 -0400

    First app with interpolation and bindings

---
+++ Udemy/Angular/D0AEA2FJSHTMLCSS/mi-primera-app/src/app/personas/personas.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html'
+  // template: '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona><app-persona></app-persona>'
+  // template: `<h1>Listado de Personas</h1>
+  // <app-persona></app-persona>
+  // <app-persona></app-persona>
+  // <app-persona></app-persona>`
+  , styleUrls: ['./personas.component.css']
+  // , styles: ['h1{ color: green; }']
+  // , styles: [`
+  //   h1 {
+  //     color: gray;
+  //   }
+  // `]
+})
+
+export class PersonasComponent {
+  agregarPersona: boolean = false;
+  agregarPersonaStatus: string = "No se ha agregado ninguna persona";
+  displayStatus: string = 'none';
+  tituloPersona: string = 'Ingeniero';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.agregarPersona = true;
+      }
+      , 5000
+    );
+  }
+
+  onCrearPersona() {
+    this.agregarPersonaStatus = "Persona agregada";
+    this.displayStatus = 'block';
+  }
+
+  onModificarPersona(event: Event) {
+    this.tituloPersona = (event.target as HTMLInputElement).value;
+  }
+}
