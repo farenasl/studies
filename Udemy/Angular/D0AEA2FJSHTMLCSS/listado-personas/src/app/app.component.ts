df65ca9a28074f2891d7ceed0bdcc38983188c7b
Fabián Arenas Loyola <farenasl@MacBook-Pro-de-Fabian.local>
2020-05-17 18:43:33 -0400

    App to binding values to object list then to GUI

---
+++ Udemy/Angular/D0AEA2FJSHTMLCSS/listado-personas/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Listado de Personas';
+  personas: Persona[] = [new Persona('Juan', 'Perez'), new Persona('Laura', 'Prieto')];
+  nombreInput: string;
+  apellidoInput: string;
+
+  onAgregarPersona() {
+    if (this.nombreInput || this.apellidoInput) {
+      // console.log(this.nombreInput + ' ' + this.apellidoInput);
+      this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
+    }
+  }
+}
