be5d840988c49823176a6949eeae0d639571f36b
Fabián Arenas Loyola <farenasl@MacBook-Pro-de-Fabian.local>
2020-05-18 09:41:02 -0400

    Adding message validator

--- Udemy/Angular/D0AEA2FJSHTMLCSS/listado-personas/src/app/app.component.ts
+++ Udemy/Angular/D0AEA2FJSHTMLCSS/listado-personas/src/app/app.component.ts
@@ -11,11 +11,16 @@ export class AppComponent {
   personas: Persona[] = [new Persona('Juan', 'Perez'), new Persona('Laura', 'Prieto')];
   nombreInput: string;
   apellidoInput: string;
+  displayStatus: string = 'none';
+  statusMessage: string = '* Debe ingresar ambos campos';
 
   onAgregarPersona() {
-    if (this.nombreInput || this.apellidoInput) {
+    if (this.nombreInput && this.apellidoInput) {
       // console.log(this.nombreInput + ' ' + this.apellidoInput);
       this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
+      this.displayStatus = 'none';
     }
+    else
+      this.displayStatus = 'block';
   }
 }
