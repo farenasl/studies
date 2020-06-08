59495b79eae4cf88f64450879212a3bef3557043
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-06-07 22:24:15 -0400

    Project creation

---
+++ Udemy/Swift/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by BCI MacBook Pro on 07-06-20.
+//  Copyright © 2020 SILCA 8 Ltda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
