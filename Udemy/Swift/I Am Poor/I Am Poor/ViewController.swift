acc8091b50135eb1f64e42af13b627c31db95af7
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-06-09 22:55:09 -0400

    I am poor app - es

---
+++ Udemy/Swift/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by BCI MacBook Pro on 09-06-20.
+//  Copyright © 2020 farenasl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
