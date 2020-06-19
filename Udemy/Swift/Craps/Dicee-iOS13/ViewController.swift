9d4adc3238846cd7cae93a148d7eb48ae5ac6544
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-06-18 21:40:31 -0400

    Randomized dices

--- Udemy/Swift/Craps/Dicee-iOS13/ViewController.swift
+++ Udemy/Swift/Craps/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray.randomElement()
     }
-
-
 }
 
---
+++ Udemy/Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//import UIKit
+
+//var str = "Hello, playground"
+
+print("Holanda")
+
+/*
+ fdsdasf
+ */
+
+print("Holanda \(1+1) veces")
+
