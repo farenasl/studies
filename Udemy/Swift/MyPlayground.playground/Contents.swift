7bafe89343e7c50dfe4c159ae0529d0fbb7a45fe
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-07 16:04:41 -0400

    if-else 2

--- Udemy/Swift/MyPlayground.playground/Contents.swift
+++ Udemy/Swift/MyPlayground.playground/Contents.swift
@@ -29,8 +29,11 @@ print("prueba2")
 func loveCalculator() {
     let loveScore = Int.random(in: 0...100)
     
-    if loveScore == 100 {
+    if loveScore > 80 {
         print("You love each other like Kanye loves Kanye")
+    }
+    else if loveScore >= 40 {
+        print("You go together like Coke and Mentos")
     }
     else {
         print("You'll be forever alone")
