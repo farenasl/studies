c7644f32a3203e3eb53d5f0521f977be4ce34ab2
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-06 21:21:30 -0400

    Final version with opacity delay

--- Udemy/Swift/Xylophone/Xylophone/ViewController.swift
+++ Udemy/Swift/Xylophone/Xylophone/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
         playSound(sender.titleLabel!.text!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
 
     func playSound(_ letter: String) {
