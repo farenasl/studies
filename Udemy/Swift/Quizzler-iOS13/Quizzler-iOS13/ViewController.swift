24cc21b7973b549227331d743ec2b7567c1a657c
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-12 20:26:26 -0400

    multiple question with answer

--- Udemy/Swift/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Udemy/Swift/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [["Four + Two is equal to Six?", "True"]
+        , ["Five - Three is greater than One?", "True"]
+        , ["Three + Eight is less than Ten?", "True"]]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
+        updateQuestion()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if sender.currentTitle == quiz[questionNumber][1]{
+            print("Right!")
+        }
+        else {
+            print("Wrong!")
+        }
+        if questionNumber < quiz[0].count - 1  {
+            questionNumber += 1
+            updateQuestion()
+        }
+    }
+    
+    func updateQuestion() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
