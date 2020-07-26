89b387d5817b05d5320dad022ab1db64277cdc43
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-26 11:28:05 -0400

    Getting/setting for sliders

--- Udemy/Swift/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ Udemy/Swift/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func HeightSliderChanged(_ sender: UISlider) {
+        heightValueLabel.text = String(NSString(format: "%.1f", sender.value)) + "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValueLabel.text = String(NSString(format: "%.0f", sender.value)) + "Kg"
+    }
 }
 
