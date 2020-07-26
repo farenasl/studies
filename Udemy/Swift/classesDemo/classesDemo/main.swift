dd0b7b2f5995a3336a9d3c52b1b3419f9f6a72f6
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-26 12:02:54 -0400

    Inherit of classes

--- Udemy/Swift/classesDemo/classesDemo/main.swift
+++ Udemy/Swift/classesDemo/classesDemo/main.swift
@@ -8,3 +8,10 @@ skeleton.attack()
 
 let skeleton2 = Enemy()
 let skeleton3 = Enemy()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords!, My claws are spears! My wings are a hurricane")
+dragon.attack()
+dragon.move()
