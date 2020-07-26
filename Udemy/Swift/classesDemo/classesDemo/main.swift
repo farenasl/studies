e159075c1b82fe18a9b8db3a4891b985eff969a6
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-26 11:44:33 -0400

    Demo project to work with classes and objects

---
+++ Udemy/Swift/classesDemo/classesDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  classesDemo
+//
+//  Created by BCI MacBook Pro on 26-07-20.
+//  Copyright © 2020 farenasl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
