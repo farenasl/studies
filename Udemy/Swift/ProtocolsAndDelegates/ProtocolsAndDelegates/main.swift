ffae9b5a565f4a8d66e457cb683a11476e99bcdb
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-08-23 10:46:28 -0400

    use of protocols and delegates implementation

---
+++ Udemy/Swift/ProtocolsAndDelegates/ProtocolsAndDelegates/main.swift
@@ -0,0 +1,59 @@
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("You can tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let pete = Paramedic(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr... ")
+    }
+}
+
+let angela = Surgeon(handler: emilio)
+emilio.assessSituation()
+emilio.medicalEmergency()
