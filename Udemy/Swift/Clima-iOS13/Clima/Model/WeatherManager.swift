98301c5e70dedbded12b610fdb09873c09a695e7
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-08-25 22:17:00 -0400

    API consuming

---
+++ Udemy/Swift/Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by BCI MacBook Pro on 25-08-20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=eccc86ad98508dbf371a8b08f2154d82&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
