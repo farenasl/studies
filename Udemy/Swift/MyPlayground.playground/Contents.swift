2dfc6ad0d1802c22c35ebee9ff8c38281dc3afad
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-08-27 22:15:32 -0400

    use of clousures

--- Udemy/Swift/MyPlayground.playground/Contents.swift
+++ Udemy/Swift/MyPlayground.playground/Contents.swift
@@ -62,3 +62,87 @@ func loveCalculator2() {
 for _ in 0...100 {
     loveCalculator2()
 }
+
+
+func calculator(n1 :Int, n2: Int) -> Int {
+    return n1 * n2
+}
+
+func add(n1 :Int, n2: Int) -> Int {
+    return n1 + n2
+}
+
+calculator(n1: 2, n2: 3)
+
+func func3(n1: Int, n2: Int, operation: (Int, Int)->Int) -> Int {
+    return operation(n1, n2)
+}
+
+func3(n1: 2, n2: 3, operation: add);
+
+func multiply(n1: Int, n2: Int) -> Int {
+    return n1*n2
+}
+func3(n1: 2, n2: 3, operation: multiply);
+
+//clousure
+func3(n1: 2, n2: 3, operation: { (n1: Int, n2: Int) -> Int in
+    return n1*n2
+})
+
+func3(n1: 2, n2: 3, operation: {(n1, n2) -> Int in
+    return n1*n2
+})
+
+func3(n1: 2, n2: 3, operation: {(n1, n2) in
+    return n1*n2
+})
+
+var result = func3(n1: 2, n2: 3, operation: {(no1, no2) in no1*no2 })
+print(result)
+//anonymous parameter names
+result = func3(n1: 2, n2: 3, operation: {$0*$1})
+print(result)
+
+result = func3(n1: 2, n2: 3) {$0*$1}
+print(result)
+
+result = func3(n1: 2, n2: 3) {$0 * $1}
+print(result)
+
+//example
+let array = [6,2,3,9,4,1]
+
+func addOne(n1: Int) -> Int {
+    return n1 + 1
+}
+
+array.map(addOne)
+
+//addOne function as a clousure
+print(array.map({(n1: Int) -> Int in
+        return n1 + 1
+    }
+))
+
+print(array.map({(n1) -> Int in
+        return n1 + 1
+    }
+))
+
+print(array.map({(n1) in
+        return n1 + 1
+    }
+))
+
+print(array.map({(n1) in
+        n1 + 1
+    }
+))
+
+print(array.map({$0 + 1}))
+
+print(array.map{$0 + 1})
+
+let newArray = array.map{"\($0)"}
+print(newArray)
