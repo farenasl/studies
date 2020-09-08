a424546df7189381c37b2d45ebdf2c6713ef8d42
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-09-07 21:12:57 -0300

    Updating async data from api rest

--- Udemy/Swift/Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Udemy/Swift/Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -53,6 +53,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         print(weather.temperature)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        
     }
     
     func didFailWithError(error: Error) {
