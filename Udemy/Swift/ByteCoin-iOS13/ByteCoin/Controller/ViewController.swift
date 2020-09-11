3037a6c6904411b5b51b7ac36ed33c890f641c1d
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-09-11 16:05:13 -0300

    Extensions for code refactoring

--- Udemy/Swift/ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ Udemy/Swift/ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class ViewController: UIViewController {
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -21,7 +21,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
     }
-    
+}
+
+//MARK: - CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
     func didUpdatePrice(price: String, currency: String) {
         DispatchQueue.main.async {
             self.bitcoinLabel.text = price
@@ -32,7 +35,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func didFailWithError(error: Error) {
         print(error)
     }
+}
 
+//MARK: - UIPickerView DataSource & Delegate
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -49,4 +55,3 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         coinManager.getCoinPrice(for: coinManager.currencyArray[row])
     }
 }
-
