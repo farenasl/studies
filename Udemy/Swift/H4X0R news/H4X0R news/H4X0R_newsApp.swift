c6514c4e8878e225506ee02816c68d2c073043eb
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-11 20:30:38 -0300

    H4X0R news SwiftUI app - initial commit

---
+++ Udemy/Swift/H4X0R news/H4X0R news/H4X0R_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_newsApp.swift
+//  H4X0R news
+//
+//  Created by BCI MacBook Pro on 11-10-20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
