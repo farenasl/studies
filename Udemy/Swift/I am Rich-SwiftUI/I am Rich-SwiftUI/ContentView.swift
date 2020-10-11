4f6779fa2747f9d7209ebf642a1afbb960db4cfb
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-11 18:51:22 -0300

    Initial commit

---
+++ Udemy/Swift/I am Rich-SwiftUI/I am Rich-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich-SwiftUI
+//
+//  Created by BCI MacBook Pro on 07-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I am Rich")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
