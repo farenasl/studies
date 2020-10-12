d1b87b78ca4f03b834c29c1858ed1776fc3f71df
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-11 21:00:01 -0300

    Receiving api data and displaying it

--- Udemy/Swift/H4X0R news/H4X0R news/ContentView.swift
+++ Udemy/Swift/H4X0R news/H4X0R news/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -24,8 +32,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Hola"),
-    Post(id: "3", title: "Bonjour")
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Hola"),
+//    Post(id: "3", title: "Bonjour")
+//]
--- Udemy/Swift/H4X0R news/H4X0R news/Models/NetworkManager.swift
+++ Udemy/Swift/H4X0R news/H4X0R news/Models/NetworkManager.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-class NetworkManager {
+class NetworkManager: ObservableObject {
+    @Published var posts = [Post]()
+    
     func fetchData() {
         if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
             let session = URLSession(configuration: .default)
@@ -16,7 +18,10 @@ class NetworkManager {
                     let decoder = JSONDecoder()
                     if let safeData = data {
                         do {
-                            try decoder.decode(Results.self, from: safeData)
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
                         } catch {
                             print(error)
                         }
