abe7918acc22cee0c60a179d45b2b803755525d1
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-16 16:35:04 -0300

    Object support for userdefault use

--- Udemy/Swift/MyPlayground.playground/Contents.swift
+++ Udemy/Swift/MyPlayground.playground/Contents.swift
@@ -216,3 +216,21 @@ func fibonacci(n: Int) {
 }
 
 fibonacci(n: 5)
+
+let defaults = UserDefaults.standard
+
+let dictionaryKey = "myDictionary"
+
+defaults.set(0.24, forKey: "Volume")
+defaults.set(true, forKey: "MusicOn")
+defaults.set("Angela", forKey: "PlayerName")
+defaults.set(Date(), forKey: "AppLastOpenedByUser")
+let array1 = [1, 2, 3]
+defaults.set(array1, forKey: "myArray")
+let dictionary = ["name": "Faby"]
+defaults.set(dictionary, forKey: dictionaryKey)
+
+let volume = defaults.float(forKey: "Volume")
+let appLastOpen = defaults.object(forKey: "AppLastOpenedByUser")
+let myArray = defaults.array(forKey: "myArray") as! [Int]
+let myDictionary = defaults.dictionary(forKey: dictionaryKey)
