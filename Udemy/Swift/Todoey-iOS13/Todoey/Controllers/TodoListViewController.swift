99502abeca37990277176a109ea5701ec20b327c
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-19 20:52:48 -0300

    Fixing reusable cell problem

--- Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    var itemArray = ["Find smthng", "Fix some table", "Do something"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem1 =  Item()
+        newItem1.title = "Find smthng"
+        newItem1.done = true
+        itemArray.append(newItem1)
+        
+        let newItem2 =  Item()
+        newItem2.title = "Find smthng"
+        itemArray.append(newItem2)
+        
+        let newItem3 =  Item()
+        newItem3.title = "Find smthng"
+        itemArray.append(newItem3)
 
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -29,7 +42,10 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -37,12 +53,9 @@ class TodoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -53,7 +66,9 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
---
+++ Udemy/Swift/Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by BCI MacBook Pro on 19-10-20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
