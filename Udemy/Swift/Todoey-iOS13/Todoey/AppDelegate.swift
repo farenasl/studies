db859e82c0911d35415b8f2e37ccb9442a259100
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-27 20:24:39 -0300

    Saving data in realm

--- Udemy/Swift/Todoey-iOS13/Todoey/AppDelegate.swift
+++ Udemy/Swift/Todoey-iOS13/Todoey/AppDelegate.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        let data = Data()
+        data.name = "Fabián"
+        data.age = 32
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
         return true
     }
 
