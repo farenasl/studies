fc932067aba5d0d4b4f67dc593aba828d7894134
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-28 12:36:29 -0300

    update data with realm

--- Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -48,11 +48,16 @@ class TodoListViewController: UITableViewController {
     
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
-//        todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-//
-//        saveItems()
-        
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write{
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status, \(error)")
+            }
+        }
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
