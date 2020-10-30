82aec4af53569d1dd9c0fd82a54ee54a4fcf6b5d
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-10-30 16:06:54 -0300

    Adding gradient color to the items sub-list

--- Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Udemy/Swift/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeTableViewController {
     let realm = try! Realm()
@@ -23,6 +24,7 @@ class TodoListViewController: SwipeTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(dataFilePath)
+        tableView.separatorStyle = .none
     }
 
     //MARK - Tableview Datasource Methods
@@ -35,6 +37,11 @@ class TodoListViewController: SwipeTableViewController {
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
+            if let colour = FlatSkyBlue().darken(byPercentage: CGFloat(Float(indexPath.row)/Float(todoItems!.count))) {
+                cell.backgroundColor = colour
+                cell.textLabel?.textColor = ContrastColorOf(colour, returnFlat: true)
+            }
+            
             cell.accessoryType = item.done ? .checkmark : .none
         } else {
             cell.textLabel?.text = "No items added"
