b94c6c6c346e41a5c55d8ebb1c0c621c76654cdd
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-11-07 13:04:28 -0300

    Computed properties example

--- Udemy/Swift/Advanced.playground/Contents.swift
+++ Udemy/Swift/Advanced.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
 var str = "Hello, playground"
+
+let pizzaInInches: Int = 12
+var numberOfSlices: Int = 10
+
+print(numberOfSlices)
+
+var numberOfSlicesComputed: Int {
+    return pizzaInInches - 4
+}
+
+print(numberOfSlicesComputed)
+
+var numberOfSlicesComputed2: Int {
+    get {
+        return pizzaInInches - 4
+    }
+}
+
+print(numberOfSlicesComputed2)
+
+let a = numberOfSlicesComputed * 2
+
+func calcPizzaSlices() {
+    numberOfSlices = pizzaInInches - 4
+}
+
+calcPizzaSlices()
+print(numberOfSlices)
+
